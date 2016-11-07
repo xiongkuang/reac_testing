055e29ff5fd7d713787d39034a27df5711b56404
xiongkuang <xiongkuang@gmail.com>
2016-11-07 00:03:40 -0500

    first

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,14 @@
-import { renderComponent , expect } from '../test_helper';
+import {renderComponent, expect} from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+// Use 'describe' to group together similar tests
+describe('App')
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+
+//Use 'it' to test a single attribute of a target
+it('shows the correct text')
+
+
+// Use 'expect' to make an 'assertion' about a target
+expect
